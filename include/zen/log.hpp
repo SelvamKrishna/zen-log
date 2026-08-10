22f1e4924e4b26adac1eb928016395d39642c511
Selvam Krishna <selvam.krishna0510@gmail.com>
2026-08-10 11:16:14 +0800

    feat: improved user space
    - feat: logger class for better configure logging
    - fix: proper ansi colored output based on ostream
    - `log_process` to trace and log a functions report
    - panic function overhaul

--- include/zen/log.hpp
+++ include/zen/log.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
-#include "zen/log/ansi.hpp"  // IWYU pragma: keep
-#include "zen/log/core.hpp"  // IWYU pragma: keep
-#include "zen/log/debug.hpp" // IWYU pragma: keep
+#include "zen/log/tools.hpp"  // IWYU pragma: keep
+#include "zen/log/ansi.hpp"   // IWYU pragma: keep
+#include "zen/log/core.hpp"   // IWYU pragma: keep
+#include "zen/log/debug.hpp"  // IWYU pragma: keep
